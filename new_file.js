da17328b64f32ce0af5aa7e3193235e007e0cd89
wumingxiaozu10 <14171088@qq.com>
2019-11-09 21:08:05 +0800

    git具体使用步骤

--- new_file.js
+++ new_file.js
@@ -1,7 +1,19 @@
-//git 使用命令
-//先cd 文件
-//$ git add .  或者  $ git add "test.rtf"
-//$ git commit -m "描述文字"
+//git 使用命令 先去git官网下载并安装,具体查看GitHub帮助文档：
+			//https://help.github.com/cn/github/getting-started-with-github/set-up-git
+
+//1.先cd 文件
+
+//2.创建仓库 $ git init
+
+//3. 添加文件
+// 添加全部文件：$ git add .
+// 或者 添加指定文件： $ git add "test.rtf"
+
+// 4.提交
+// $ git commit -m "xxx"
+
+//5.加入到哪个库
 //$ git remote add origin https://github.com/wumingxiaozu10/-.git
-//git push -u origin master
-console.log("修改方法")
+
+//6.确认usename 和pawsord
+//$ git push -u origin master
