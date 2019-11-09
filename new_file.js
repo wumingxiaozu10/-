c81fe1f72688ef20d0003da547a3f507c0c5ab65
wumingxiaozu10 <14171088@qq.com>
2019-11-09 20:49:53 +0800

    git的使用

--- new_file.js
+++ new_file.js
@@ -1,1 +1,7 @@
-console.log("First init ")
+//git 使用命令
+//先cd 文件
+//$ git add .  或者  $ git add "test.rtf"
+//$ git commit -m "描述文字"
+//$ git remote add origin https://github.com/wumingxiaozu10/-.git
+//git push -u origin master
+console.log("hello")
